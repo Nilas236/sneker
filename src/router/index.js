0bbc1a2cee9e9b8cc7853f49771175faf834263b
Nilas236 <49131141+Nilas236@users.noreply.github.com>
2022-06-26 19:50:17 +0200

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from "vue-router";
+import ProductList from "../views/ProductList.vue";
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/products",
+  },
+  {
+    path: "/products",
+    name: "Products",
+    component: ProductList,
+  },
+  {
+    path: "/products/:prodId",
+    name: "Detail",
+    props: true,
+    component: () => import("../views/ProductDetail.vue"),
+  },
+  {
+    path: "/checkout",
+    name: "Checkout",
+    component: () => import("../views/TheCheckout.vue"),
+  },
+  {
+    path: "/:notFound(.*)",
+    name: "Not Found",
+    component: () => import("../views/NotFound.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/store/modules/products.js
@@ -0,0 +1,41 @@
+export default {
+  state() {
+    return {
+      products: [],
+    };
+  },
+  mutations: {
+    setProducts(state, payload) {
+      state.products = payload;
+    },
+  },
+  actions: {
+    async loadProducts(context) {
+      const response = await fetch(
+        "https://snkrs-shop-default-rtdb.firebaseio.com/products.json"
+      );
+      const responseData = await response.json();
+      const prods = [];
+      if (!response.ok) {
+        const err = new Error(responseData.message || "Error");
+        throw err;
+      }
+      for (let key in responseData) {
+        prods.push({
+          id: key * 1,
+          name: responseData[key].name,
+          color_way: responseData[key].color_way,
+          description: responseData[key].description,
+          price: Math.floor(responseData[key].price) * 60,
+          image: responseData[key].image,
+        });
+      }
+      context.commit("setProducts", prods);
+    },
+  },
+  getters: {
+    getProducts(state) {
+      return state.products;
+    },
+  },
+};
